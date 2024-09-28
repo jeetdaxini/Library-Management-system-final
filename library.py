a197845a8fb146bd22f5370dd27ba0f6f9150e78
jeetdaxini <jeetdaxini925@gmail.com>
2024-09-28 16:12:07 +0530

    Implement Book_borrow Functionality

--- library.py
+++ library.py
@@ -3,5 +3,10 @@ class Library:
         self.books = {}
 
     def add_book(self, isbn, title, author, year):
-        book = {'isbn': isbn, 'title': title, 'author': author, 'year': year}
+        book = {'isbn': isbn, 'title': title, 'author': author, 'year': year, 'borrowed': False}
         self.books[isbn] = book
+
+    def borrow_book(self, isbn):
+        if self.books[isbn]['borrowed']:
+            raise ValueError("Book is already borrowed.")
+        self.books[isbn]['borrowed'] = True
