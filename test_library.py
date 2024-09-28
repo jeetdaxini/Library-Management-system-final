db6d882894feaca4b8b124694e83cfd3bbb057c6
jeetdaxini <jeetdaxini925@gmail.com>
2024-09-28 16:02:49 +0530

    Add test for borrowing a book

---
+++ test_library.py
@@ -0,0 +1,15 @@
+import pytest
+from library import Library
+
+def test_add_book():
+    library = Library()
+    library.add_book('978-3-16-148410-0', 'Test Book', 'Author', 2023)
+    assert len(library.books) == 1
+    assert library.books['978-3-16-148410-0'] ['title'] == 'Test Book'
+
+def test_borrow_book():
+    library = Library()
+    library.add_book('978-3-16-148410-0', 'Test Book', 'Author', 2023)
+    library.borrow_book('978-3-16-148410-0')
+    assert library.books['978-3-16-148410-0']['borrowed'] is True
+
